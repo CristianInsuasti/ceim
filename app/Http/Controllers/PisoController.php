0801ec009015d6deb7e3aaa10d2ccd6a587424b6
Cristian Steven Insuasti Mora <ceim243@gmail.com>
2020-01-31 14:35:05 -0500

    Insertar datos con tinker en Edificio, Piso, Area

--- app/Http/Controllers/PisoController.php
+++ app/Http/Controllers/PisoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Piso;
 use Illuminate\Http\Request;
 
 class PisoController extends Controller
@@ -13,7 +14,7 @@ class PisoController extends Controller
      */
     public function index()
     {
-        //
+        return Piso::all();
     }
 
     /**
